e4a67aa9c34821b5a652d2d5685083ceff106559
DelpeyratR <roxane.delpeyrat@orange.fr>
2016-02-15 11:48:25 +0100

    ajout recuit
    Le recuit marche enfin. A tester sur des grosses instances

---
+++ src/recuit.cpp
@@ -0,0 +1,274 @@
+#include <stdio.h>
+#include <stdlib.h>
+#include <time.h>  
+
+#include "voisinage.h"
+#include <iostream>
+#include <fstream>
+#include <string>
+#include <vector>
+#include <math.h>
+#include <algorithm>
+using namespace std;
+
+
+carte Ha,Hp,Ca,Cp;
+int n,m;
+void getData(ifstream& fichier) {
+	char char1;
+	double tmp;
+	if(fichier) {
+		//pour n
+		fichier >> char1 >> char1 >> n >> char1;
+		cout <<"n="<< n<<"\n";
+		//pour m
+		fichier >> char1 >> char1 >> m >>char1;
+		cout <<"m="<< m<<"\n";
+		int Ba,Bp;
+		//pour Ba, si existe!
+		fichier >> char1 ;
+		if(char1=='B'){//then Ba est définie, et Bp aussi
+		  fichier >> char1 >> char1>>Ba >>char1;
+		  cout <<"Ba="<< Ba<<"\n";
+
+		  //pour Bp
+		  fichier >> char1 >> char1 >>char1 >> Bp >>char1;
+		
+		  
+		// Ha
+		fichier >> char1 >> char1 >> char1 >> char1; // lit H a = [       
+		}
+		else if(char1=='H'){	fichier >> char1 >> char1 >> char1; // le 'H' a été lu, on lit " a = ["
+		}
+		else cout << "Problème de lecture de l'instance, données non conforme au modèle...\n";
+		
+
+		
+		for(int i=0;i<n;i++) {
+			fichier >> char1 ; // lit le [ de la ligne
+			vector<float> ligne;; // On initialise la ième ligne de M
+			for(int j=0;j<m;j++) {
+				fichier >> tmp >> char1;
+				// Chaque ligne du fichier contient n fois le motif suivant :
+				// un entier suivi d’un caractère ("," ou "]" a la fin)
+				ligne.push_back(tmp); // On ajoute l’entier lu à la ième ligne de M
+			}
+			Ha.push_back(ligne);
+			ligne.clear();
+			fichier >> char1;//lit ",", Fin de la lecture de la ième ligne de M
+		}
+		fichier >> char1; //lit ; a la fin de E
+		displayData(Ha,n,m);
+		fichier >> char1 >> char1 >> char1 >> char1; // lit C a = [
+
+		for(int i=0;i<n;i++) {
+			fichier >> char1 ; // lit le [ de la ligne
+			vector<float> ligne; // On initialise la ième ligne de M
+			for(int j=0;j<m;j++) {
+				fichier >> tmp >> char1;
+				// Chaque ligne du fichier contient n fois le motif suivant :
+				// un entier suivi d’un caractère ("," ou "]" a la fin)
+				ligne.push_back(tmp); // On ajoute l’entier lu à la ième ligne de M
+			}
+			Ca.push_back(ligne);
+			ligne.clear();
+			fichier >> char1;//lit ",", Fin de la lecture de la ième ligne de M
+		}
+		fichier >> char1; //lit ; a la fin du tableau
+		displayData(Ca,n,m);
+
+		// Hp
+		
+		fichier >> char1 >> char1 >> char1 >> char1; // lit C a = [
+
+		for(int i=0;i<n;i++) {
+			fichier >> char1 ; // lit le [ de la ligne
+			vector<float> ligne;
+			for(int j=0;j<m;j++) {
+				fichier >> tmp >> char1;
+				// Chaque ligne du fichier contient n fois le motif suivant :
+				// un entier suivi d’un caractère ("," ou "]" a la fin)
+				ligne.push_back(tmp); // On ajoute l’entier lu à la ième ligne de M
+			}
+			Hp.push_back(ligne);
+			ligne.clear();
+			fichier >> char1;//lit ",", Fin de la lecture de la ième ligne de M
+		}
+		fichier >> char1; //lit ; a la fin du tableau
+		displayData(Hp,n,m);
+
+		// Cp
+		
+		fichier >> char1 >> char1 >> char1 >> char1; // lit C p = [
+
+		for(int i=0;i<n;i++) {
+			fichier >> char1 ; // lit le [ de la ligne
+			vector<float> ligne; // On initialise la ième ligne de M
+			for(int j=0;j<m;j++) {
+				fichier >> tmp >> char1;
+				// Chaque ligne du fichier contient n fois le motif suivant :
+				// un entier suivi d’un caractère ("," ou "]" a la fin)
+				ligne.push_back(tmp); // On ajoute l’entier lu à la ième ligne de M
+			}
+			Cp.push_back(ligne);
+			ligne.clear();
+			fichier >> char1;//lit ",", Fin de la lecture de la ième ligne de M
+		}
+		fichier >> char1; //lit ; a la fin du tableau
+		displayData(Ca,n,m);
+
+		fichier.close();
+	}
+}
+solution voisinage(solution s)
+{
+	bool chgt=false;
+	solution svoisin=s;
+	int compteur=0;
+	while(chgt==false&&compteur<20){
+	chgt=false;
+	svoisin=voisinage1(s,&chgt,Ha,Hp,Ca,Cp,n,m);
+	compteur++;
+	}
+	return svoisin;
+}
+solution recuit(solution& init,ofstream& sortie,int r,float Tinit,float phi,int Kmax)
+{
+	//paramètres
+	time_t tic,tac;
+	double tps;
+	solution max=init;
+	solution s=init;
+	solution ss=init;
+	float T=Tinit;
+	int K=0;
+	float delta;
+	float q;
+	float e;
+	sortie<<"Paramètres"<<endl;
+	sortie<<"Tinit = "<<Tinit<<endl<<"phi ="<<phi<<endl;
+	sortie<<"Kmax = "<<Kmax<<endl<<"r = "<<r<<endl;
+	while(K<Kmax)
+	{
+		tic=time(NULL);
+		for(int nbIter=0;nbIter<r;nbIter++)
+		{
+			//on tire au sort une solution s'
+			ss=voisinage(s);
+			//display(ss.z,n,m);
+			delta=ss.taille-s.taille;
+			if(delta>=0&&ss.quotient>=2)//la nouvelle solution a une énergie plus basse
+			{
+				s=ss;
+				if(s.taille>max.taille){
+					max=s;}
+			}
+			else
+			{
+				//On tire un nombre q pour décider si on garde la solution ss ou pas
+				q=rand()%1000;
+				q=q/1000;
+				e=exp(delta/T);
+				if(q<=e){
+					s=ss;
+				}
+			}
+		}
+		tac=time(NULL);
+		tps=difftime(tac,tic);
+		sortie<<"Itération = "<<K<<endl;
+		sortie<<"Temperature = "<<T<<endl;
+		sortie<<"Temps de l'éxecution = "<<tps<<endl;
+		sortie<<"Meileure solution trouvée jusqu'à présent = "<<max.taille<<endl;
+		sortie<<"Quotient de la meilleure solution trouvee = "<<max.quotient<<endl;
+		K++;
+		//Changement de température
+		T=T*phi;
+	}
+	return max;
+}
+solution initialisation(int n,int m)
+{
+	solution sol;
+	bool find=false;
+	for(int i=0;i<n;i++){
+		vector<int> ligne;
+		for(int j=0;j<m;j++) 
+		{ 
+			if((Ha[i][j]+Hp[i][j]>=2)&&find==false)
+			{
+				ligne.push_back(1);
+				pair<int,int> p;
+				p.first=i;
+				p.second=j;
+				sol.interieur.push_back(p);
+				sol.exterieur=voisins(i,j,n,m);
+				sol.taille=1;
+				sol.quotient=Ha[i][j]+Hp[i][j];
+				find=true;
+			}
+			else
+				ligne.push_back(0);
+		}
+		sol.z.push_back(ligne);
+	}
+	if(find)
+		cout<<"solution initiale admissible"<<endl;
+	else
+		cout<<"erreur"<<endl;
+	display(sol.z,n,m);
+	return sol;
+
+}
+int main ()
+{
+	time_t deb,fin;
+	double tps;
+	string s;
+	string t;
+	string rep;
+	srand (time(NULL));
+	
+	/*cout<< "entrez nom du fichier:"<<endl;
+	cin>>s;
+	cout<<"entrez nom du fichier resultat:"<<endl;
+	cin>>t;*/
+	rep="C:/Users/J.pages/Documents/Roxane/ProjetECMA-master/ProjetECMA-master/instances/instances_eleves/";
+	t="a";
+	s="projet_5_8_1";
+	t=rep+t+".txt";
+	ofstream sortie;
+	sortie.open(t, ios::out); 
+	s=rep+s+".dat";
+	ifstream fichier(s, ios::in); 
+		//lecture du fichier de données
+		getData(fichier);
+		solution Sinit=initialisation(n,m);
+		int r=10; //nombre d'itérations par palier de température
+		float Tinit=100; //température
+		float phi=0.9;//facteur de décroissance
+		int Kmax=10;//nombre de paliers de température
+/*
+		cout<< "Choisissez une temperature initiale Tinit:"<<endl;
+		cin>>Tinit;
+		cout<< "Choisissez un nombre de paliers de temperature Kmax:"<<endl;
+		cin>>Kmax;
+		cout<< "Choisissez le nombre d'iterations sur un meme palier r:"<<endl;
+		cin>>r;
+		cout<< "Choisissez un facteur de decroissance de la température phi:"<<endl;
+		cin>>phi;*/
+		//Calcul de la solution initiale
+		//zone Sinit=initialisation(n,m);
+		//Calcul de la solution par recuit simulé
+		//tps=difftime(fin,deb);
+		solution  Srecuit;
+		deb=time(NULL);
+		Srecuit=recuit(Sinit,sortie,r,Tinit,phi,Kmax);
+		fin=time(NULL);
+		tps=difftime(fin,deb);
+		//sortie<<"Valeur finale de la solution par recuit simulé = "<<Srecuit.z<<endl;
+		sortie<<"Temps d'éxécution total = "<<tps;
+		
+		
+	}
+			
---
+++ src/voisinage.cpp
@@ -0,0 +1,314 @@
+#include "voisinage.h"
+
+using namespace std;
+
+void display(zone sol,int n, int m){
+	for(int i=0;i<n;i++)
+	{
+		cout<<"[ ";
+		for(int j =0;j<m;j++)
+		{
+			cout<<sol[i][j]<<" ";
+		}
+		cout<<"]"<<endl;
+	}
+	cout<<endl;
+}
+void displayData(carte data,int n, int m){
+	for(int i=0;i<n;i++)
+	{
+		cout<<"[ ";
+		for(int j =0;j<m;j++)
+		{
+			cout<<data[i][j]<<" ";
+		}
+		cout<<"]"<<endl;
+	}
+	cout<<endl;
+}
+bool frontiere(zone sol,int a,int b,int n,int m){
+	if(a==0||a==n)
+	{
+		if(b==0||b==m)
+			return true;
+	}
+	else{
+		int valeur=sol[a][b];
+		//test si la case est sur un bord ie si un de ses voisins haut/bas/droite/gauche est à une valeur différente
+		bool bord=false;
+		if(b>0){
+			if(sol[a][b-1]!=valeur)
+				bord=true;}
+		if(b<m-1){
+			if(sol[b][b+1]!=valeur)
+				bord=true;}
+
+		if(bord==false&&a!=0){
+			if(sol[a-1][b]!=valeur)
+				bord=true;}
+		if(bord==false&&a<n-1){
+			if(sol[a+1][b]!=valeur)
+				bord=true;}
+		return bord;
+	}
+}
+zone arbre(zone graphe, int x, int y,int n,int m)
+{
+	if(graphe[x][y]==1)
+	{
+		graphe[x][y]=2;
+		//cout<<"examen du noeud "<<x<<" "<<y<<endl;
+		//display(graphe);
+		if(x!=0)
+		{
+			if(graphe[x-1][y]==1)
+				graphe=arbre(graphe,x-1,y,n,m);
+		}
+		if(x!=n-1)
+		{
+			if(graphe[x+1][y]==1)
+				graphe=arbre(graphe,x+1,y,n,m);
+		}
+		if(y!=0)
+		{
+			if(graphe[x][y-1]==1)
+				graphe=arbre(graphe,x,y-1,n,m);
+		}
+		if(y!=m-1)
+		{
+			//cout<<x<<" "<<y<<endl<<"graphe[x][y+1]="<<graphe[x][y+1]<<endl;
+			if(graphe[x][y+1]==1)
+				graphe=arbre(graphe,x,y+1,n,m);
+		}
+	}
+	return graphe;
+}
+bool connexite(zone sol,int n,int m)
+{
+	//on cree le graphe correspondant à la sol et on teste si ce graphe est connexe ou non
+	int xDeb,yDeb;
+	bool deb=true;
+	zone graphe;
+	vector<int> colonne;
+	bool connexe=true;
+	for(int i=0;i<n;i++)
+	{
+		for(int j=0;j<m;j++)
+		{
+			colonne.push_back(sol[i][j]);
+			if(sol[i][j]==1&&deb){
+				xDeb=i;
+				yDeb=j;
+				deb=false;
+			}
+		}
+		graphe.push_back(colonne);
+		colonne.clear();
+	}
+	//display(graphe);
+	graphe=arbre(graphe,xDeb,yDeb,n,m);
+	//display(graphe);
+	for(int i=0;i<n;i++)
+	{
+		for(int j=0;j<m;j++)
+		{
+			if(graphe[i][j]==1)
+			{
+				connexe=false;
+				break;
+			}
+		}
+	}
+	return connexe;
+}
+vector<pair<int,int>> voisins(int a,int b,int n,int m)
+{
+	vector<pair<int,int>> v;
+	pair<int,int> p;
+	p.first=a;
+	p.second=b;
+	if(a!=0){
+		p.first--;
+		v.push_back(p);
+		p.first++;
+	}
+	if(a!=n-1){
+		p.first++;
+		v.push_back(p);
+		p.first--;
+	}
+	if(b!=0){
+		p.second--;
+		v.push_back(p);
+		p.second++;
+	}
+	if(b!=m-1){
+		p.second++;
+		v.push_back(p);
+		p.second--;
+	}
+	return v;
+}
+float admissible(zone s, carte Ha, carte Ca, carte Hp, carte Cp,int n,int m)
+{
+	float numa=0;
+	float nump=0;
+	float denoma=0;
+	float denomp=0;
+	for(int i=0;i<n;i++)
+	{
+		for(int j=0;j<m;j++)
+		{
+			numa=numa+Ha[i][j]*Ca[i][j]*s[i][j];
+			nump=nump+Hp[i][j]*Cp[i][j]*s[i][j];
+			denoma=denoma+Ca[i][j]*s[i][j];
+			denomp=denomp+Cp[i][j]*s[i][j];
+		}
+	}
+	return (numa/denoma)+(nump/denomp);
+}
+float score(zone s,int n,int m)
+{
+	float compteur=0;
+	for(int i=0;i<n;i++)
+	{
+		for(int j=0;j<m;j++)
+		{
+			compteur=compteur+s[i][j];
+		}
+	}
+	return compteur;
+}
+solution voisinage1(solution s,bool* chgt,carte Ha,carte Hp,carte Ca,carte Cp,int n,int m)
+{//on tire un chiffre n au hasard entre 0 et taille interieur +taille exterieur
+	//on permute la valeur la n-ieme case de la liste (interieur,exterieur)
+	//on teste si la connexite est preservee et on recalcule le score et le quotiennt
+	//on met à jour interieur et exterieur
+	solution ss=s;
+	int t_int=s.interieur.size(),t_ext=s.exterieur.size(),c;
+
+	c=rand()%(t_int+t_ext);
+	if(t_int==1&&c==0)
+		c++;
+	int a,b;
+	if(c<t_int)
+	{
+		a=ss.interieur[c].first;
+		b=ss.interieur[c].second;
+	}
+	else
+	{
+		c=c-t_int;
+		a=ss.exterieur[c].first;
+		b=ss.exterieur[c].second;
+	}
+	//cout<<"a et b intialises a= "<<a<<" b= "<<b<<endl;
+	if(Hp[a][b]+Ha[a][b]!=0)//on permute si la case est possible
+	{
+		ss.z[a][b]=(ss.z[a][b]+1)%2;
+		*chgt=true;
+
+		if(ss.z[a][b]==0)//si la case est passee à zero il faut verifier la connexite et mettre a jour interieur et exterieur
+		{
+			bool connexe=connexite(s.z,n,m);
+			if(connexe)
+			{
+				//on met a jour l'exterieur et l'interieur
+				vector<pair<int,int>> casesVoisines=voisins(a,b,n,m);
+				int x,y;
+				for(int i=0;i<casesVoisines.size();i++)
+				{
+					x=casesVoisines[i].first;
+					y=casesVoisines[i].second;
+					//cout<<"acces case (x,y) x= "<<x<<" y= "<<y<<endl;
+					if(ss.z[x][y]==0)//la case fait partie de l'ancien exterieur
+					{
+						vector<pair<int,int>> v_xy=voisins(x,y,n,m);
+						int compteur=0;
+						for(int j=0;j<v_xy.size();j++)
+						{
+							//cout<<"acces case voisine (x,y) i= "<<v_xy[i].first<<" j= "<<v_xy[i].second<<endl;
+							if(ss.z[v_xy[j].first][v_xy[j].second]==1)
+								compteur++;
+						}
+						if(compteur==0)
+							//la case n'a plus de voisins dans la solution donc elle ne fait plus partie de l'exterieur
+						{
+							for(int j=0;j<ss.exterieur.size();j++)
+							{
+								if(ss.exterieur[j].first==x&&ss.exterieur[j].second==y)
+								{
+									ss.exterieur.erase(ss.exterieur.begin()+j);
+									break;
+								}
+							}
+						}
+
+					}
+				}
+				//on passe la case dont on a change la valeur de interieur vers exterieur
+				for(int j=0;j<ss.interieur.size();j++)
+				{
+					if(ss.interieur[j].first==a&&ss.interieur[j].second==b)
+					{
+						ss.interieur.erase(ss.interieur.begin()+j);
+						break;
+					}
+				}
+				pair<int,int> p;
+				p.first=a;
+				p.second=b;
+				ss.exterieur.push_back(p);
+				ss.taille--;
+			}
+			else
+			{
+				ss.z[a][b]=(ss.z[a][b]+1)%2;
+				*chgt=false;
+
+			}
+
+		}
+		else//ss.z[a][b]==1
+		{
+			vector<pair<int,int>> v=voisins(a,b,n,m);
+			for(int i=0;i<v.size();i++)
+			{
+				//cout<<"acces case voisin (a,b) x= "<<v[i].first<<" y= "<<v[i].second<<endl;
+				if(ss.z[v[i].first][v[i].second]==0)//on verifie que la case est sur l'exterieur sinon on l'ajoute
+				{
+					bool find=false;
+					for(int j=0;j<ss.exterieur.size();j++)
+					{
+						if(ss.exterieur[j].first==v[i].first&&ss.exterieur[j].second==v[i].second)
+						{
+							find=true;
+							break;
+						}
+					}
+					if(!find)
+						ss.exterieur.push_back(v[i]);
+				}
+			}
+			for(int j=0;j<ss.exterieur.size();j++)
+				{
+					if(ss.exterieur[j].first==a&&ss.exterieur[j].second==b)
+					{
+						ss.exterieur.erase(ss.exterieur.begin()+j);
+						break;
+					}
+				}
+				pair<int,int> p;
+				p.first=a;
+				p.second=b;
+				ss.interieur.push_back(p);
+				ss.taille++;
+		}
+	}
+	else
+		*chgt=false;
+	ss.quotient=admissible(ss.z,Ha,Ca,Hp,Cp,n,m);
+	ss.taille=score(ss.z,n,m);
+return ss;
+}
+
